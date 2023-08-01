8d8b98e4ac3782581fe372db9f1023e8f3e91ee9
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-07-31 23:33:16 -0300

    Criando Migrations

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using api.Data;
+using API_ASP.NET.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace API_ASP.NET
